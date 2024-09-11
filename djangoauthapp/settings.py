aa31913b09556c2160e60fe3eda64b95136f0e69
upendraupi <upendrakuruma@gmail.com>
2024-09-11 23:12:01 +0530

    secondcommit2

--- djangoauthapp/settings.py
+++ djangoauthapp/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='default-secret-key')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
